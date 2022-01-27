ec529330f0ee737171b49b0edcd2a3b925d748ff
Gurveer Parmar <gurveerparmar92@gmail.com>
2022-01-28 04:23:21 +0700

    added JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/userModel');
 
@@ -42,6 +43,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -54,8 +56,31 @@ const registerUser = asyncHandler(async (req, res) => {
 
 // wrapped with asynchandler - handle promises
 const loginUser = asyncHandler(async (req, res) => {
-  res.send('Login Route');
+  // pull email and password
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  // check user and passwords match
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid credentials');
+  }
 });
+
+//Generate JWT token = expires in 30days
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
 
 module.exports = {
   registerUser,
