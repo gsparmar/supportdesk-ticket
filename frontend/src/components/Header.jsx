6e67088a3487f0cf878adc317cd39c07be7ffb29
Gurveer Parmar <gurveerparmar92@gmail.com>
2022-02-01 23:03:23 +0700

    added logout

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,20 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
 import { Container, Navbar, Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { reset, logout } from '../features/auth/authSlice';
 
 function Header() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate('/');
+  };
   return (
     <header>
       <Navbar bg='light' variant='light'>
@@ -11,16 +23,24 @@ function Header() {
             <Navbar.Brand>Support Desk</Navbar.Brand>
           </LinkContainer>
           <Nav className='ms-auto'>
-            <LinkContainer to='/login'>
+            {user ? (
               <Nav.Link>
-                <FaSignInAlt /> Login
+                <button onClick={onLogout}>Logout</button>
               </Nav.Link>
-            </LinkContainer>
-            <LinkContainer to='/register'>
-              <Nav.Link>
-                <FaUser /> Register
-              </Nav.Link>
-            </LinkContainer>
+            ) : (
+              <>
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    <FaSignInAlt /> Login
+                  </Nav.Link>
+                </LinkContainer>
+                <LinkContainer to='/register'>
+                  <Nav.Link>
+                    <FaUser /> Register
+                  </Nav.Link>
+                </LinkContainer>
+              </>
+            )}
           </Nav>
         </Container>
       </Navbar>
--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -35,6 +35,11 @@ export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
   console.log(user);
 });
 
+// logout user
+export const logout = createAsyncThunk('/', async () => {
+  await authService.logout();
+});
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
@@ -61,6 +66,9 @@ export const authSlice = createSlice({
         state.user = null;
         state.isError = true;
         state.message = action.payload;
+      })
+      .addCase(logout.fulfilled, (state) => {
+        state.user = null;
       });
   },
 });
