6ca26768d60ee0f4606f097c33ae0474af9294f2
Gurveer Parmar <gurveerparmar92@gmail.com>
2022-02-10 19:16:10 -0800

    prep for deploy

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// root url - message
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to the Support Desk API' });
-});
-
 // routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
@@ -29,10 +24,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
   // send html file from build folder
-  app.get('*', (req, res) =>
+  app.get('*', (_, res) =>
     res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
   );
 } else {
+  // root url - message
   app.get('/', (req, res) => {
     res.status(200).json({ message: 'Welcome to the Support Desk API' });
   });
